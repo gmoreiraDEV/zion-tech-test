1dd46bcb423b9aebb231d1b078e0b96935ed14c8
Guilherme Moreira <guilhermemoreira.web@gmail.com>
2025-07-02 13:48:48 -0300

    fix: usePosts hooks

--- hooks/usePostsWithCommentCount.ts
+++ hooks/usePostsWithCommentCount.ts
@@ -2,9 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { IPost } from "@/lib/types";
 import { getAllPosts } from "@/data-access-layer/posts.dal";
 
-export function usePostsWithCommentCount() {
+const LIMIT = 5;
+
+export function usePostsWithCommentCont() {
   return useQuery<IPost[], Error>({
     queryKey: ["posts"],
-    queryFn: getAllPosts,
+    queryFn: ({ pageParam }) => getAllPosts(pageParam as number, LIMIT),
   });
 }
